60e805036b5104bfe619cd48e6dbb92f08da894f
shukvip <47320761+shukvip@users.noreply.github.com>
2019-04-05 17:52:45 +0530

    Add files via upload

---
+++ JPA With Hibernate/Demos/Demos/JPAStarter/src/com/cg/jpastart/entities/Student.java
@@ -0,0 +1,44 @@
+package com.cg.jpastart.entities;
+
+
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+
+
+
+
+@Entity
+//@Table(name="students")
+public class Student implements Serializable {
+	
+	
+
+	@Id
+	//@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "USERS_SEQ")
+    @SequenceGenerator(name = "USERS_SEQ", sequenceName = "SEQUENCE_USERS",allocationSize=10)
+	private int studentId;
+	@Column(length=20)
+	private String name;
+	public int getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
